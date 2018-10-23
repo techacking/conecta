ad1930c86c5f307d3789855c5af1d024ba916151
VictorKiill <30813666+VictorKiill@users.noreply.github.com>
2018-10-23 17:27:18 -0300

    Add files via upload
    Visualização de Eventos na agenda
    PS: Não Testado Completamente

--- agenda/views.py
+++ agenda/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import *
+import json
 
 @login_required
 def agenda(request):
-    entrada=Entre.objects.all()
-    return render(request, 'agenda.html')
+    eventos = Orcamento.objects.all()
+	eventosList = []
+	for evento in eventos:
+		eventoDict = {
+			"title": str(evento.contato),
+			"startdate": str(evento.dataini),
+			"enddate": str(evento.dataterm),
+		}
+		eventosList.append(eventoDict)
+    return render(request, 'agenda.html', {'eventosList': json.dumps(eventosList)})
